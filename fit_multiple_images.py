b48e38b0b7dac6293cf3d9ee4cedffb573b767c4
Michael Rabinovich <michael@commonground-ai.com>
2020-04-22 11:23:37 +0300

    Fitting multiple images save images at the output folder, numerically sorted

--- fit_multiple_images.py
+++ fit_multiple_images.py
@@ -9,9 +9,11 @@ import cv2
 from psbody.mesh import Mesh
 from psbody.mesh.meshviewer import MeshViewers
 from fitting.landmarks_fitting import *
+import shutil
+import errno
 import time
 
-def fit_flame_to_images(images, texture_mapping, input_folder, out_path, load_shape_path, save_shape):
+def fit_flame_to_images(images, texture_mapping, out_path, load_shape_path, save_shape):
     if not os.path.exists(out_path):
         os.makedirs(out_path)
 
@@ -25,7 +27,7 @@ def fit_flame_to_images(images, texture_mapping, input_folder, out_path, load_sh
         shape_params_np = np.load(load_shape_path)
         flamelayer.set_shape(shape_params_np)
 
-    init_image = cv2.imread(os.path.join(input_folder,images[0]))
+    init_image = cv2.imread(images[0])
 
     # Predict face location once (#important! Will not work well when face moves a lot)
     face_detector, face_landmarks_predictor = get_face_detector_and_landmarks_predictor()
@@ -68,8 +70,7 @@ def fit_flame_to_images(images, texture_mapping, input_folder, out_path, load_sh
     # add smoothness/temporal consistency
     flamelayer.set_laplacian(vertices,faces)
     first_fit = True
-    for img in images:
-        target_img_path = os.path.join(input_folder,img)
+    for target_img_path in images:
         print ('Fitting image at ', target_img_path)
         target_img = cv2.imread(target_img_path)
         time_before = time.time()
@@ -97,6 +98,22 @@ def save_mesh(result_mesh, result_scale, out_path, target_img_path):
     write_obj(result_mesh, out_mesh_fname)
     np.save(os.path.join(out_path, os.path.splitext(os.path.basename(target_img_path))[0] + '_scale.npy'), result_scale)
 
+
+def video_to_images(video_path, max_iter):
+    cap = cv2.VideoCapture(config.input)
+    i = 0
+    images = []
+    while (cap.isOpened() and i < max_iter):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret == True:
+            images.append(frame)
+            i = i+1
+        # Break the loop
+        else: 
+            break
+    return images
+
 def save_images_in_video(images, input_folder, output_folder, image_viewpoint_ending):
     video_name = output_folder + '/' + 'video-' + image_viewpoint_ending + '.avi'
     frame = cv2.imread(os.path.join(input_folder,images[0]))
@@ -108,13 +125,34 @@ def save_images_in_video(images, input_folder, output_folder, image_viewpoint_en
     cv2.destroyAllWindows()
     video.release()
 
+def load_data_and_copy_to_output_folder(inp, image_viewpoint_ending, output_folder, max_images):
+    output_file_paths = [os.path.join(output_folder, os.path.basename(inp) + str(i) + '.png') for i in range(max_images)]
+    # Get all images
+    if os.path.isdir(inp):
+        input_images = [os.path.join(inp,img) for img in os.listdir(inp) if img.endswith(image_viewpoint_ending)]
+        input_images.sort()
+        input_images = input_images[:max_images]
+        output_file_paths = output_file_paths[:len(input_images)]
+        for i in range(len(output_file_paths)):
+            shutil.copyfile(input_images[i], output_file_paths[i])
+
+    elif os.path.isfile(inp):
+        frames = video_to_images(inp, max_images)
+        output_file_paths = output_file_paths[:len(frames)]
+        for i in range(len(output_file_paths)):
+            cv2.imsave(output_file_paths[i], frames[i])
+    else:
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config.input)
+
+    return output_file_paths
 
 if __name__ == '__main__':
-    parser.add_argument('--input_folder', help='Path of the input folder images')
+    parser.add_argument('--input', help='Path of the input folder for images, or path of video file')
     parser.add_argument('--output_folder', help='Output folder path')
     parser.add_argument('--image_viewpoint_ending',default='26_C.jpg', help='Ending of the file from the given angle')
     parser.add_argument('--texture_mapping',type = str,default = './data/texture_data.npy',help = 'Texture data')
     parser.add_argument('--load_shape_path', type = str,default = '', help = 'Load shape from a given path')
+    parser.add_argument('--max_images', type = int,default = 200, help='Maximum number of images to fit')
     
     # With shape matching before or without
     parser.add_argument('--save_shape', dest='save_shape', action='store_true')
@@ -123,12 +161,10 @@ if __name__ == '__main__':
     config.batch_size = 1
     config.flame_model_path = './model/male_model.pkl'
 
-    # Get all images
-    images = [img for img in os.listdir(config.input_folder) if img.endswith(config.image_viewpoint_ending)]
-    images.sort()
+    images_p = load_data_and_copy_to_output_folder(config.input, config.image_viewpoint_ending, config.output_folder, config.max_images)
 
     # uncomment the following to create a movie from the raw images (not reconstruction) 
     #save_images_in_video(images, config.input_folder, config.output_folder, config.image_viewpoint_ending)
 
     # Iteratively fit flame to images
-    fit_flame_to_images(images, config.texture_mapping, config.input_folder, config.output_folder, config.load_shape_path, config.save_shape)
+    fit_flame_to_images(images_p, config.texture_mapping, config.output_folder, config.load_shape_path, config.save_shape)
