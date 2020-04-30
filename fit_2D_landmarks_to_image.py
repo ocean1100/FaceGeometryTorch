7232322f3c0022e5a9944c296b0260358b76538e
Michael Rabinovich <michael@commonground-ai.com>
2020-04-30 15:32:57 +0300

    Added edge plotting and debug mode for visualization

--- fit_2D_landmarks_to_image.py
+++ fit_2D_landmarks_to_image.py
@@ -5,9 +5,9 @@ import os
 from psbody.mesh.meshviewer import MeshViewers
 from fitting.landmarks_fitting import *
 from fitting.silhouette_fitting import *
+from utils.render_mesh import *
 
-
-def fit_geometry_and_texture_to_2D_landmarks(texture_mapping, target_img_path, out_path):
+def fit_geometry_and_texture_to_2D_landmarks(texture_mapping, target_img_path, out_path, dbg):
     if not os.path.exists(target_img_path):
         print('Target image not found - s' % target_img_path)
         return
@@ -47,7 +47,12 @@ def fit_geometry_and_texture_to_2D_landmarks(texture_mapping, target_img_path, o
     out_texture_img_fname = os.path.join(out_path, os.path.splitext(os.path.basename(target_img_path))[0] + '.png')
     result_mesh = get_weak_perspective_textured_mesh(vertices, faces, target_img, texture_mapping, result_scale,
                                                      out_texture_img_fname)
-    save_and_display_results(result_mesh, result_scale, out_path, target_img_path)
+    if (dbg):
+        _, landmarks_3D, _ = flamelayer()
+        proj_landmarks = torch_project_points_weak_perspective(landmarks_3D, scale)
+        display_debug_output(result_mesh, result_scale, target_img, target_2d_lmks, proj_landmarks.detach().cpu().numpy().squeeze())
+    else:
+        save_and_display_results(result_mesh, result_scale, out_path, target_img_path)
 
 
 def save_and_display_results(result_mesh, result_scale, out_path, target_img_path):
@@ -59,6 +64,38 @@ def save_and_display_results(result_mesh, result_scale, out_path, target_img_pat
     mv[0][0].set_static_meshes([Mesh(result_mesh.v, result_mesh.f)])
     mv[0][1].set_static_meshes([result_mesh])
 
+
+def display_debug_output(mesh, scale, target_img, plt_target_lmks, landmarks_3D):
+    # Render the mesh
+    rendered_img = render_mesh(Mesh(scale*mesh.v, mesh.f), height=target_img.shape[0], width=target_img.shape[1])
+
+    # show edges
+    edges_src = cv2.Canny(target_img,0,200)
+    edges_mesh = cv2.Canny(rendered_img,0,200)
+    edges = np.hstack((edges_src, edges_mesh))
+    cv2.imshow('edges', edges)
+    edges_overlay = np.zeros(target_img.shape, dtype = np.uint8)
+    edges_overlay[:,:,0] = edges_src
+    edges_overlay[:,:,2] = edges_mesh
+    cv2.imshow('edges_overlay', edges_overlay)
+
+    plt_target_lmks[:, 1] = target_img.shape[0] - plt_target_lmks[:, 1]
+    for (x, y) in plt_target_lmks:
+        cv2.circle(target_img, (int(x), int(y)), 4, (0, 0, 255), -1)
+
+    plt_opt_lmks = landmarks_3D.copy()
+    plt_opt_lmks[:,1] = target_img.shape[0] - plt_opt_lmks[:,1]
+    for (x, y) in plt_opt_lmks:
+        cv2.circle(target_img, (int(x), int(y)), 4, (255, 0, 0), -1)
+
+    for (x, y) in plt_opt_lmks:
+        cv2.circle(rendered_img, (int(x), int(y)), 4, (255, 0, 0), -1)
+    target_img = np.hstack((target_img, rendered_img))
+    # show landmarks
+    cv2.imshow('img', target_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
 
 if __name__ == '__main__':
     parser.add_argument(
@@ -79,11 +116,13 @@ if __name__ == '__main__':
         default='./data/texture_data.npy',
         help='Texture data')
 
+    parser.add_argument('--debug', dest='debug', action='store_true')
+
     config = get_config()
     config.batch_size = 1
     config.flame_model_path = 'model/male_model.pkl'
 
     print('Running 2D landmark fitting')
-    fit_geometry_and_texture_to_2D_landmarks(config.texture_mapping, config.target_img_path, config.out_path)
+    fit_geometry_and_texture_to_2D_landmarks(config.texture_mapping, config.target_img_path, config.out_path, config.debug)
 
 
